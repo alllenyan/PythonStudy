676d189ec453a3bea0a7cf3bed0b905c16bb7c07
Allen Yan <yanzhanghong1016@gmail.com>
2020-04-15 11:02:39 +0800

    add test99 program

---
+++ test_99.py
@@ -0,0 +1,29 @@
+#打印*矩阵
+j = 0
+while j < 5:
+    j = j + 1
+    i = 0
+    while i < 5:
+        i = i + 1
+        print('*', end=' ')
+    print()
+
+#打印*三角形
+j = 0
+while j < 5:
+    j = j + 1
+    i = 0
+    while i < j:
+        i = i + 1
+        print('*', end=' ')
+    print()
+
+#打印9*9乘法表
+j = 0
+while j < 9:
+    j = j + 1
+    i = 0
+    while i < j:
+        i = i + 1
+        print(i,'*',j,'=',i*j,end=' ')
+    print()
